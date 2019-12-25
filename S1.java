07a589f809e99fed78e0be76a8c1f1eb3f0d2c51
sonali mahavar <53106053+sonali-mahavar@users.noreply.github.com>
2019-12-25 11:00:43 +0530

    Add files via upload

---
+++ S1.java
@@ -0,0 +1,13 @@
+
+package s1;
+import java.util.Scanner;
+public class S1 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("ENTER ANY NUMBER:");
+        int num=sc.nextInt();
+        System.out.println("The number enter by the user is:"+num);
+    }
+    
+}
